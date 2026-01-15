9090db754bc1270a83c13a40f49cb25deb7e70e6
Art Morozov <art.morozov@solace.com>
2026-01-15 11:34:54 -0500

    update

--- sam-slack-gateway-adapter/src/sam_slack_gateway_adapter/handlers.py
+++ sam-slack-gateway-adapter/src/sam_slack_gateway_adapter/handlers.py
@@ -12,6 +12,7 @@ from typing import TYPE_CHECKING, Any, Awaitable, Callable, Dict
 from solace_agent_mesh.gateway.adapter.types import ResponseContext
 from . import utils
 
+
 if TYPE_CHECKING:
     from .adapter import SlackAdapter
 
